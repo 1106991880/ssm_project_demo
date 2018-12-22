0294688ce63ea89796eefa988b1a0a55f68f10df
1106991880 <1106991880@qq.com>
2018-12-22 13:30:55 +0800

    Update TestImgPassCodeApi.java

--- src/main/java/cn/springmvc/zstudytest/TestImgPassCodeApi.java
+++ src/main/java/cn/springmvc/zstudytest/TestImgPassCodeApi.java
@@ -7,7 +7,7 @@ package cn.springmvc.zstudytest;
  * 参考地址http://www.360doc.com/content/18/1217/13/61278929_802402078.shtml
  */
 public class TestImgPassCodeApi {
-    //12306图片验证码接口,打开链接就能访问,测试修改了git远程仓库的代码之后,本地idea怎么查看
+    //12306图片验证码接口,打开链接就能访问,测试修改了git远程仓库的代码之后,本地idea怎么查看1
     //url = "https://kyfw.12306.cn/otn/passcodeNew/getPassCodeNew?module=other&rand=sjrand";
 
 }
